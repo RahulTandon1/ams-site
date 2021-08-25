16488c750c59cb159ca4b3b11d0ea93330f4311d
Rahul Tandon <jesuisrahult@gmail.com>
2021-08-25 13:11:40 +0530

    maybe menu mobile fix

--- src/pages/newLanding.js
+++ src/pages/newLanding.js
@@ -60,7 +60,7 @@ const NewHomePage = () => {
     return(
         <React.Fragment>
         <div className="menu-container">
-            <div className="fadeout" onClick={ closeNav }></div>
+            <div className="fadeout" onClick={ closeNav } onTouchStart={closeNav}></div>
             <div className="menu">
                 <div class="yellow-highlight"></div>
                 <h1 class="school-name-heading">Amrit Masonic Special School</h1>
@@ -118,7 +118,7 @@ const NewHomePage = () => {
             {/* <a href="#"> </a>*/}
             
             <div class="header">
-                <div class="menu-button" onClick={ openNav}>
+                <div class="menu-button" onClick={ openNav} onTouchStart={openNav}>
                     <svg class="hamburger" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
                     <span class="menu-indicating-text">MENU</span>                
                 </div>
